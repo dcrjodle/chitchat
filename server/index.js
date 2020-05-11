0b3ec5b9b5888ae5fde9f4f6bdfd28e93c0ca323
dcrjodle <joel.bystedt@gmail.com>
2020-05-11 12:20:45 +0200

    inte klar än

--- server/index.js
+++ server/index.js
@@ -1,18 +1,81 @@
-const express = require('express')
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const bodyParser = require('body-parser')
-const socket = require("socket.io")
 const {Users, Messages} = require('./sequelize')
 const cors = require("cors");
 
-const app = express();
+
+
+
 
 var corsOptions = {
   origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
-
 app.use(bodyParser.json())
+
+
+
+
+io.on('connect', (socket) => {
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if(error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+    }
+  })
+});
+
+/*io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});*/
+
+
+
+
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
 
 /* create a sub
 app.post('/api/subs', (req, res) => {
@@ -73,35 +136,6 @@ app.put('/api/subs/postalCode/:postalCode?', (req, res) => {
   })
 })*/
 
-const port = 3000
-app.listen(port, () => {
-    console.log(`Running on http://localhost:${port}`)
-})
-
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-  });
-});
-
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-});
-
-http.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+});*/
