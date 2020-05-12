0f5904841af10774e6f4ff57c979f197cde3d7f0
Alice <oi17alm@cs.umu.se>
2020-05-12 12:51:10 +0200

    Grund till client

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App'
+//Behövs för varje react application
+ReactDOM.render(<App/>, document.querySelector('#root'));
+
+
+
