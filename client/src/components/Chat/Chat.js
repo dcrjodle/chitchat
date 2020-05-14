06c3aad4f32c0e0b28e5a27a4b98878fb56300a8
Alice <oi17alm@cs.umu.se>
2020-05-14 14:01:26 +0200

    welcome malin, inf0bar added

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,8 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
+import './Chat.css';
 
 let socket;
+
+
 const Chat = ({location}) => {
     const[name, setName]= useState('');
     const[room, setRoom]= useState('');
@@ -57,6 +60,7 @@ const Chat = ({location}) => {
         <div className="generalOuter">
             <h1 className="heading">Chat</h1>
             <div className="general">
+                <InfoBar name={name}/>
             <input value={message} onChange={(event) => setMessage(event.target.value)}
             onKeyPress={(event=> event.key === 'Enter'? sendMessage(event): null)}/>
 
---
+++ client/src/components/Chat/InfoBar/infoBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//import icons
+
+import './InfoBar.css';
+
+const InfoBar = ({name}) =>{
+<div className="InfoBar">
+    <div className='leftInnerContainer'>
+        <img className='onlineIcon' src={onlineIcon} alt="online image"> </img>
+        <h3>Welcome {name} !</h3>
+    </div>
+    <div className='rightInnerContainer'>
+        <a href="/"> <img src={closeIcon} alt="close image"></img></a>
+    </div>
+</div>
+}
+
+export default InfoBar;
