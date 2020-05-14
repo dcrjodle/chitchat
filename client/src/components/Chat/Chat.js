73f6eaa6f65703c90a08302777e019806086ab71
Alice <oi17alm@cs.umu.se>
2020-05-14 13:26:36 +0200

    sendMessage tag implemented

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -5,8 +5,12 @@ import io from 'socket.io-client';
 let socket;
 const Chat = ({location}) => {
     const[name, setName]= useState('');
-    const[room, setRoom]= useState('')
-    const ENDPOINT = 'localhost:3001';
+    const[room, setRoom]= useState('');
+    const[messages, setMessages]= useState([]);
+    const[message, setMessage]= useState('');
+
+
+    const ENDPOINT = 'localhost:3000';
 
     useEffect(() => {
         const {name, room} = queryString.parse(location.search);
@@ -27,10 +31,37 @@ const Chat = ({location}) => {
 
         //console.log(socket);
     }, [ENDPOINT, location.search])
+
+    useEffect(() =>{
+        socket.on('message', (message) => {
+            setMessages([...messages, message]);
+
+        })
+        }, [messages]);
+
+        const sendMessage = (event)=>{
+
+            event.preventDefault();
+
+
+            if(message){
+                socket.emit('sendMessage', message, ()=> setMessage(''));
+            }
+        }
+
+        console.log(message, messages);
+
+        //function for sending messages
+
     return (
         <div className="generalOuter">
             <h1 className="heading">Chat</h1>
-            <div className="general">hejsAN</div>
+            <div className="general">
+            <input value={message} onChange={(event) => setMessage(event.target.value)}
+            onKeyPress={(event=> event.key === 'Enter'? sendMessage(event): null)}/>
+
+            </div>
+
         </div>
     )
 }
