cb660247fbf239844ced83c97b2f7f6c0f26227d
Malin <malin.forsgren@icloud.com>
2020-05-18 13:14:50 +0200

    Message and messages added

---
+++ client/src/components/Message/Message.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import ScrollToBottom from 'react-scroll-to-bottom'
+
+import "./Messages.css";
+
+const Message = ({ message, name}) => {
+    let isSentBycurrentUser = false;
+
+    const trimmedName = name.trim().toLowerCase();
+
+    if (user === trimmedName) {
+        isSentBycurrentUser = true;
+    }
+
+    return {
+        isSentBycurrentUser
+           ? (
+            <div className="messageContainer justifyEnd">
+                <p className="sentText">(trimmedName)</p>
+                <div className="messageBox backgroundBlue">
+                <p className="messageText colorWhite">(text)</p>
+                </div>
+            </div>
+            )
+            : (
+            <div className="messageContainer justifyStart">
+                <div className="messageBox backgroundLight">
+                <p className="messageText colorDark">(text)</p>
+                </div>
+            <p className="sentText pl-10">{user}</p>
+            </div>
+
+            )
+    }
+}
+
+export default Messages;
---
+++ client/src/components/Messages/Messages.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import ScrollToBottom from 'react-scroll-to-bottom'
+
+import "./Message.css";
+
+const Input = ({ message, setMessage, sendMessage}) => {
+
+}
+
+export default Message;
